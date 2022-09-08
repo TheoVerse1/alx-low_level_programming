a31268082551766556adb04e561e108b95ba2cd0
TheoVerse1 <pohambadaniel@gmail.com>
2022-09-08 15:40:05 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - using put function to print the String
+ *
+ * Return will be 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
