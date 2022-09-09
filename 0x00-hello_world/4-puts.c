a01af34bc3558a1437c032ffc09672e6fcc59f16
TheoVerse1 <pohambadaniel@gmail.com>
2022-09-09 12:27:08 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 /**
  * main - prints the string in the put funtction
  *
- * Description: Write a C program that prints, exactly "Programming is like building a multilingual puzzle. followed by a new line".
+ * Description: Write a C program that prints put
  *
  * Return: Always 0 (Success)
  */
