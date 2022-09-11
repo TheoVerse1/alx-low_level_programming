378f9670ea003b4c4aa1f5885d071d2b57df2906
TheoVerse1 <pohambadaniel@gmail.com>
2022-09-11 12:42:28 -0700

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
-int main(void){
+/**
+ *main - print String and follow with a new line
+ *
+ * Description: using puts function
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
