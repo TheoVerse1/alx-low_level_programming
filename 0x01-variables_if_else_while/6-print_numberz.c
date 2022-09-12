b054050cf7dd92da8bd39922586de9613bfdcb04
TheoVerse1 <pohambadaniel@gmail.com>
2022-09-12 13:03:30 -0700

    6. Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,7 +3,7 @@
 #include <time.h>
 
 /**
- * main - Write a program that prints all single digit numbers of base 10 starting from 0
+ * main - Write a program that prints single digit numbers of base 10 starting 0
  *
  * Return: Always 0 (Success)
  */
