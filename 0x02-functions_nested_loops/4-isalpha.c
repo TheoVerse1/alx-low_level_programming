db9a8bb449854deef0a13c21f008ca238b9b0f3b
TheoVerse1 <pohambadaniel@gmail.com>
2022-09-13 14:29:38 -0700

    4. isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * int _isalpha - function that checks for alphabetic character
+ *
+ * Return: 1 if c is a letter, lowercase or uppercase 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	char low, up;
+
+	for (low = 'a'; low <= 'z'; low++)
+	{
+		for (up = 'A'; up <= 'Z'; up++)
+		{
+			if (c == low || c ++ up)
+				return (1);
+		}
+	}
+
+	return(0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,9 +4,9 @@
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int _islower(int b);
-int _isalpha(int d);
-int print_sign(int s);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
