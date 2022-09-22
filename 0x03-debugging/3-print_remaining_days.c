41dae8dad768f486a3d807534797e960a4b6a417
TheoVerse1 <pohambadaniel@gmail.com>
2022-09-22 14:09:54 -0700

    11. It is the addition of strangeness to beauty that constitutes the romantic character in art

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,37 +1,25 @@
 #include <stdio.h>
 #include "main.h"
+#include <time.h>
 
 /**
- * print_remaining_days - takes a date and prints how many days are
- * left in the year, taking leap years into account
- * @month: month in number format
- * @day: day of month
- * @year: year
- * Return: void
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
-
-void print_remaining_days(int month, int day, int year)
+int positive_or_negative(void)
 {
-	if (year % 4 == 0 || (year % 400 == 0 && !(year % 100 == 0)))
-	{
-		if (month >= 2)
-		{
-			day++;
-		}
+	int n;
 
-		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 366 - day);
-	}
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
 	else
-	{
-		if (month == 2 && day == 60)
-		{
-			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-		}
-		else
-		{
-			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 365 - day);
-		}
-	}
+		printf("%d is negative\n", n);
+
+	return (0);
 }
