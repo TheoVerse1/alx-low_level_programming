f4c9a9c7d5608d1e858e583e79529f79d458728a
TheoVerse1 <pohambadaniel@gmail.com>
2022-10-12 11:57:47 -0700

    2. To hell with circumstances; I create opportunities

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,35 +1,29 @@
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * main - prints opcode of own main function
- * @argc: argument count
- * @argv: array of arguments
- * Return: 1 or 2 on fail, 0 on success
+ * int_index - a function that searches for an integer.
+ * @array: A pointer to array
+ * @size: Size of the array
+ * @cmp: cmp(function) to compare the elements throw array
+ * Return: The index of the first element for witch the cmp function
+ * does not return 0; -1 if no elements matches or size <= 0.
  */
-int main(int argc, char *argv[])
+int int_index(int *array, int size, int (*cmp)(int))
 {
-	int bytes, i;
-	unsigned char *func_ptr;
+	int i = 0;
 
-	if (argc != 2)
-	{
-		printf("Error\n");
-		exit(1);
-	}
-	bytes = atoi(argv[1]);
-	if (bytes < 0)
-	{
-		printf("Error\n");
-		exit(2);
-	}
-	func_ptr = (unsigned char *)main;
-	i = 0;
-	if (bytes > 0)
-	{
-		while (i < (bytes - 1))
-			printf("%02hhx ", func_ptr[i++]);
-		printf("%hhx\n", func_ptr[i]);
-	}
-	return (0);
+	if (cmp == NULL || array == NULL)
+		return (-1);
+
+	if (size <= 0)
+		return (-1);
+
+	for (; i < size; i++)
+		if (cmp(array[i]))
+			return (i);
+
+	if (i == size)
+		return (-1);
+
+	return (-1);
 }
