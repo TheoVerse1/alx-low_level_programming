2d4ac3f6717b3f6b3e19549c4d6120666f416334
TheoVerse1 <pohambadaniel@gmail.com>
2022-10-27 13:06:23 -0700

    0. 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,33 +1,48 @@
 #include "main.h"
+/**
+ * _strlen - gets length of string for a const
+ * @b: pointer to string
+ * Return: length of string
+ */
+int _strlen(const char *b)
+{
+	/*declare vairables to be used*/
+	int count = 0;
+
+	while (b[count] != '\0')
+		count++;
+	return (count);
+}
 
 /**
- * binary_to_uint - Entry Point
- * @b: const char
- * Return: 0
+ * binary_to_uint - convert binary number to unsigned int
+ * @b: pointer to a string
+ * Return: unisgned int
  */
+
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int res = 0;
-	int base = 1, i = 0;
-
-	if (b == NULL)
+	/*declare variables to be used*/
+	unsigned int self_add = 1, decimal = 0, num = 0;
+	int len, i;
+	/*check if character is not NULL*/
+	if (!b)
 		return (0);
-
-	while (b[i + 1])
+	/*get string length*/
+	len = _strlen(b);
+	/*loop through characters and check if not 0 or 1*/
+	for (i = len - 1; i >= 0; i--)
 	{
 		if (b[i] != '0' && b[i] != '1')
-			return (0);
-		i++;
+			return (0);/*there's a wrong character*/
+		/*check and convert*/
+		if (b[i] == '1')
+			num = self_add;
+		else
+			num = 0;
+
+		decimal += num;/*adds the decimal number*/
+		self_add += self_add;/*add self*/
 	}
-
-	while (i >= 0)
-	{
-		res += ((b[i] - '0') * base);
-		base *= 2;
-		i--;
-	}
-
-
-	return (res);
-
+	return (decimal);
 }
