1d4268c3e63522c847b392ceff471185c02a720d
TheoVerse1 <pohambadaniel@gmail.com>
2022-10-27 13:17:07 -0700

    2. 10

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,19 +1,17 @@
 #include "main.h"
 
 /**
- * get_bit - Entry Point
- * @n: input
- * @index: index
- * Return: 0
+ * get_bit - prints the bit at index
+ * @n: number being changed to base two
+ * @index: the index number which we want to get
+ * Return: bit at index
  */
+
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int bits;
-
-	bits = (n >> index) & 1;
-
-	if ((bits == 1) || (bits == 0))
-		return (bits);
-	else
-		return (-1);
+	/*check if index is part of n*/
+	if (index >= (sizeof(n) * 8))
+		return (-1);/* error index is not part of bit*/
+	/*index is in bit*/
+	return ((n >> index) & 1);/* get the bit at that index*/
 }
