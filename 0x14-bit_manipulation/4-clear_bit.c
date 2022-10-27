1c8c586ac8b8f2466e393f336bb9f6e0cde34087
TheoVerse1 <pohambadaniel@gmail.com>
2022-10-27 13:26:52 -0700

    4. 100

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,22 +1,20 @@
 #include "main.h"
 
 /**
- * clear_bit - Entry Point
- * @n: input
- * @index: index
- * Return: 0
+ * clear_bit - set the bit at index to 0
+ * @n: number being changed to base two
+ * @index: the index number which we want to get
+ * Return: 1 if works and or -1
  */
+
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int bits;
-
-	if (n == NULL)
-		return (-1);
-
-	bits = 1 << index;
-
-	if ((bits | *n) == *n)
-		*n = *n ^ bits;
-
+	unsigned int num = 1;
+	/*check if index is part of n*/
+	if (index >= (sizeof(n) * 8))
+		return (-1);/* error index is not part of bit*/
+	/*check if the index bit is 0 or not*/
+	if ((*n >> index) & num)/*enters loop if index bit is 1*/
+		*n ^= (num << index);/*changes 1 to 0*/
 	return (1);
 }
