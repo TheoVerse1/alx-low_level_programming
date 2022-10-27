cbc6b2824118512128e64f80b4bb91af4c5f0728
TheoVerse1 <pohambadaniel@gmail.com>
2022-10-27 13:24:12 -0700

    3. 11

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * set_bit - Entry Point
- * @n: input
- * @index: index
- * Return: 0
+ * set_bit - set the bit at index to 1
+ * @n: number being changed to base two
+ * @index: the index number which we want to get
+ * Return: bit at index
  */
+
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int bits;
-
-	if (n == NULL)
-		return (-1);
-
-	bits = 1 << index;
-
-	*n = *n | bits;
-
+	unsigned int num = 1;
+	/*check if index is part of n*/
+	if (index >= (sizeof(n) * 8))
+		return (-1);/* error index is not part of bit*/
+	/*move num by index*/
+	num <<= index;
+	/*comparing the values and changing value at n*/
+	*n = *n | num;
 	return (1);
 }
