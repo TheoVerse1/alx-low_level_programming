498d613f3b43f48d30945a71cbed373538debb68
TheoVerse1 <pohambadaniel@gmail.com>
2023-09-13 00:22:44 -0700

    0. >>> ht = {}

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,12 +16,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (size == 0)
 		return (NULL);
 
-	new_table = (hash_table_t *)malloc(sizeof(hash_table_t));
+	new_table = (hash_table_t *)calloc(sizeof(hash_table_t));
 
 	if (new_table == NULL)
 		return (NULL);
 
-	new_table->array = (hash_node_t **)malloc(sizeof(hash_node_t *) * size);
+	new_table->array = (hash_node_t **)calloc(sizeof(hash_node_t *) * size);
 	if (new_table->array == NULL)
 	{
 		free(new_table);
